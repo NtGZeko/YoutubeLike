341a0044a69678818c7d4d4211372fa15e14e3fc
NtGZeko <matheooz@live.fr>
2022-07-20 20:21:09 +0200

    init global style

--- frontend/src/GlobalStyle.jsx
+++ frontend/src/GlobalStyle.jsx
@@ -1,6 +1,45 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-background: red;
+
+:root{
+  --primary-color: #1F203E;
+  --secondary-color: #726395;
+  --tertiary-color: #2D2E46;
+  --hover-color: #31295C;
+  --font-family: 'Lato', sans-serif;
+  background-color: var(--primary-color);
+}  
+h1,h2,h3,h4,h5,h6, p, li{
+  font-family: var(--font-family);
+  color: #F8F8FF;
+}
+h1{
+  font-size: 2em;
+  font-weight: bolder;
+}
+h2{
+  font-size: 1.5em;
+  font-weight: bolder;
+}
+h3{
+  font-size: 1.17em;
+  font-weight: bolder;
+}
+h4{
+  font-size: 1em;
+  font-weight: bolder;
+}
+h5{
+font-size: .83em;
+font-weight: bolder;
+}
+h6{
+  font-size: .67em;
+  font-weight: bolder;
+}
+@media screen and (max-width: 700px) {
+    width: 90vw;
+}
  `;
 export default GlobalStyle;
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -3,6 +3,12 @@ import SHome from "./style";
 export default function Home() {
   return (
     <SHome>
+      <h1>h1</h1>
+      <h2>h2</h2>
+      <h3>h3</h3>
+      <h4>h4</h4>
+      <h5>h5</h5>
+      <h6>h6</h6>
       <p>Salut</p>
     </SHome>
   );
